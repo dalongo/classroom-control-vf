324fdfec30f031fa4ee86776db9333bbc1ee7fcc
dalongo <dalongo@adelphia.net>
2016-02-24 10:21:36 -0500

    adding nginx manifests init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,36 @@
+class ngonx {
+
+  package{'nginx':
+    ensure=>present,
+  }
+  
+  file {'docroot':
+    ensure => directory,
+    path   => "???",
+  }
+  
+  file{'index.html':
+    ensure => file,
+    path   => "/var/www/index.html",
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  
+  file{'config':
+    ensure  => file,
+    path    => "/etc/nginx/nginx.conf",
+    source  => 'puppet:///modules/nginx/default.conf',
+  }
+ 
+ file{'block':
+   ensure  => file,
+   path    => "/etc/nginx/conf.d/default.conf',",
+   source  => 'puppet:///modules/nginx/default.conf',
+   require => Package['nginx'],
+   notify  => Service['nginx'],
+  ]
+  
+  service{'nginx':
+    ensure  => running,
+    require  => File['docrot']
+    subscribe => [File['config'],File['block']]
+  ]
