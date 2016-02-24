aaa7a3e1e99c51772cd9b03e8eaf7e4126352ee3
dalongo <dalongo@adelphia.net>
2016-02-24 16:05:32 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,46 +1,51 @@
-class nginx {
-
-#  $nginx_dir = 'puppet:///modules/nginx'
-   $nginx_dir = '/modules/nginx'
-  
-  File {
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-  }
-  
-  package{'nginx':
-    ensure=>present,
-  }
-  
-  file {'docroot':
-    ensure => directory,
-    path   => "/var/www",
-  }
-  
-  file{'index.html':
-    ensure => file,
-    path   => "/var/www/index.html",
-    source => "$(nginx_dir)/index.html",
-  }
-  
-  file{'config':
-    ensure  => file,
-    path    => "/etc/nginx/nginx.conf",
-    source  => "$(nginx_dir)/default.conf",
-  }
+class nginx { 
+   File { 
+     owner => 'root', 
+     group => 'root', 
+     mode => '0664', 
+   } 
  
- file{'block':
-   ensure  => file,
-   path    => "/etc/nginx/conf.d/default.conf',",
-   source  => "$(nginx_dir)/default.conf",
-   require => Package['nginx'],
-   notify  => Service['nginx'],
-  }
-  
-  service{'nginx':
-    ensure  => running,
-    require  => File['docroot'],
-    subscribe => [File['config'],File['block']],
-  }
-}
+   $source = 'puppet:///modules/nginx' 
+ 
+ 
+   package { 'nginx': 
+     ensure => present, 
+     before => [File['config'],File['block']], 
+   } 
+    
+   file { 'docroot': 
+     ensure => directory, 
+     path => '/var/www', 
+   } 
+    
+   file { 'index': 
+     ensure => file, 
+     path => '/var/www/index.html', 
+     source => "${source}/index.html", 
+   } 
+      
+   file { 'config': 
+     ensure => file, 
+     path => '/etc/nginx/nginx.conf', 
+     source => "${source}/nginx.conf", 
+     require => Package['nginx'], 
+     notify => Service['nginx'], 
+   } 
+    
+   file { 'block': 
+     ensure => file, 
+     path => '/etc/nginx/conf.d/default.conf', 
+     source => "${source}/default.conf", 
+     require => Package['nginx'], 
+     notify => Service['nginx'], 
+   } 
+
+ 
+   service { 'nginx': 
+     ensure => running, 
+     require => [File['docroot'],File['index']], 
+     subscribe => [File['config'],File['block']], 
+   } 
+ 
+ 
+} 
