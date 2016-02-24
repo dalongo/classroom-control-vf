5bf662046504041430e5be68c992c91514bbe838
dalongo <dalongo@adelphia.net>
2016-02-24 17:42:10 -0500

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,23 @@
+define users::managed_user (
+  $home_base = "/home",
+  $group     = $title,
+){
+  File{
+    owner => $title,
+    group => $group,
+    mode => '0640'
+  }
+  group { $group:
+    ensure => present.
+  }
+  
+  user { $title:
+    ensure => present,
+    gid => $group,
+  }
+  
+  file { "$home_base/$title/.ssh":
+    ensure => directory,
+    mode => '0700',
+  }
+}
