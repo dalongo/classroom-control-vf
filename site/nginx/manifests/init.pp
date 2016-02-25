18de32b7fa1263af792e8cd68b40820313bda401
dalongo <dalongo@adelphia.net>
2016-02-25 14:14:33 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,51 +1,69 @@
-class nginx { 
-   File { 
-     owner => 'root', 
-     group => 'root', 
-     mode => '0664', 
-   } 
- 
-   $source = 'puppet:///modules/nginx' 
- 
- 
-   package { 'nginx': 
-     ensure => present, 
-     before => [File['config'],File['block']], 
-   } 
-    
-   file { 'docroot': 
-     ensure => directory, 
-     path => '/var/www', 
-   } 
-    
-   file { 'index': 
-     ensure => file, 
-     path => '/var/www/index.html', 
-     source => "${source}/index.html", 
-   } 
-      
-   file { 'config': 
-     ensure => file, 
-     path => '/etc/nginx/nginx.conf', 
-     source => "${source}/nginx.conf", 
-     require => Package['nginx'], 
-     notify => Service['nginx'], 
-   } 
-    
-   file { 'block': 
-     ensure => file, 
-     path => '/etc/nginx/conf.d/default.conf', 
-     source => "${source}/default.conf", 
-     require => Package['nginx'], 
-     notify => Service['nginx'], 
-   } 
-
- 
-   service { 'nginx': 
-     ensure => running, 
-     require => [File['docroot'],File['index']], 
-     subscribe => [File['config'],File['block']], 
-   } 
- 
- 
-} 
+class nginx (
+  $root = undef,
+) {
+  case $::osfamily {
+    'redhat','debian' : {
+       $package = 'nginx'
+       $owner = 'root'
+       $group = 'root'
+       # $docroot = '/var/www'
+       $confdir = '/etc/nginx'
+       $logdir = '/var/log/nginx'
+       # this will be used if we don't pass in a value
+       $default_docroot = '/var/www'
+    }
+    'windows' : {
+       $package = 'nginx-service'
+       $owner = 'Administrator'
+       $group = 'Administrators'
+       # $docroot = 'C:/ProgramData/nginx/html'
+       $confdir = 'C:/ProgramData/nginx'
+       $logdir = 'C:/ProgramData/nginx/logs'
+       # this will be used if we don't pass in a value
+       $default_docroot = 'C:/ProgramData/nginx/html'
+    }
+    default : {
+       fail("Module ${module_name} is not supported on ${::osfamily}")
+    }
+  }
+  # user the service will run as. Used in the nginx.conf.erb template
+  $user = $::osfamily ? {
+    'redhat' => 'nginx',
+    'debian' => 'www-data',
+    'windows' => 'nobody',
+  }
+  # if $root isn't set, then fall back to the platform default
+  $docroot = $root ? {
+     undef => $default_docroot,
+     default => $root,
+  }
+  File {
+     owner => $owner,
+     group => $group,
+     mode => '0664',
+  }
+  package { $package:
+     ensure => present,
+  }
+  file { [ $docroot, "${confdir}/conf.d" ]:
+     ensure => directory,
+  }
+  file { "${docroot}/index.html":
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  file { "${confdir}/nginx.conf":
+    ensure => file,
+    content => template('nginx/nginx.conf.erb'),
+    notify => Service['nginx'],
+  }
+  file { "${confdir}/conf.d/default.conf":
+    ensure => file,
+    content => template('nginx/default.conf.erb'),
+    notify => Service['nginx'],
+  }
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+}
